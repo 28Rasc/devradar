cce06aeb1a2ba21e7273603367e4ee92a9c4604f
Raul Cavalcanti <28rasc@gmail.com>
2020-01-15 10:55:08 -0300

    first commit aula 2

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const PORT = 3333;
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnihide@cluster0-aa5kv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use( express.json() );
+app.use(routes);
+
+app.listen(PORT);
